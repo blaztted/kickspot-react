d6270c9e0ebc2989117792020503ba8e12a6fd28
blaztted <bbc.silva@gmail.com>
2024-03-28 17:09:01 +0000

    added useState for active <hr> and styled it

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "../assets/images/logos/snake-logo.png";
+import logo from "../assets/images/logos/jordandraw.png";
 import cart_icon from "../assets/images/icons/shopping-cart.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>KICKSPOT</p>
+        {/* <p>KICKSPOT</p> */}
       </div>
       <ul className="nav-menu">
-        <li>
+        <li
+          className={menu === "home" ? "active" : ""}
+          onClick={() => {
+            setMenu("home");
+          }}
+        >
           Home
-          <hr />
+          {menu === "home" ? <hr /> : <></>}
         </li>
-        <li>Loja</li>
-        <li>Blog</li>
-        <li>Contacts</li>
+        <li
+          className={menu === "shop" ? "active" : ""}
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Loja {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          className={menu === "blog" ? "active" : ""}
+          onClick={() => {
+            setMenu("blog");
+          }}
+        >
+          Blog {menu === "blog" ? <hr /> : <></>}
+        </li>
+        <li
+          className={menu === "contacts" ? "active" : ""}
+          onClick={() => {
+            setMenu("contacts");
+          }}
+        >
+          Contacts {menu === "contacts" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
