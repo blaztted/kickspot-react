f28d413a35b706a6a2547993392d65198d194c7b
blaztted <bbc.silva@gmail.com>
2024-03-28 16:39:17 +0000

    styled navbar with css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../assets/images/logos/snake-logo.png";
+import cart_icon from "../assets/images/icons/shopping-cart.png";
 
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>KICKSPOT</p>
+      </div>
+      <ul className="nav-menu">
+        <li>
+          Home
+          <hr />
+        </li>
+        <li>Loja</li>
+        <li>Blog</li>
+        <li>Contacts</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
